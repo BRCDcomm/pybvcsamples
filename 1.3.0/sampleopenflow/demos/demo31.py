c0a0515d80aa7a04037a2a7849d4aecfd1fd26a9
jeb@elbrys.com <jeb@elbrys.com>
2015-08-17 13:00:25 -0400

    Update to match version 1.3.2 of pybvc

--- 1.3.0/sampleopenflow/demos/demo31.py
+++ 1.3.0/sampleopenflow/demos/demo31.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python
 
 """
-Copyright (c) 2015
+Copyright (c) 2015,  BROCADE COMMUNICATIONS SYSTEMS, INC
+
+All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
- - Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
--  Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
--  Neither the name of the copyright holder nor the names of its contributors
-   may be used to endorse or promote products derived from this software
-   without specific prior written permission.
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES;LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+
 
 @authors: Sergei Garbuzov
+@status: Development
+@version: 1.1.0
+
 
 """
 
@@ -94,13 +104,13 @@ if __name__ == "__main__":
     priority = 600
     cookie = 1000
     
-    match_in_port = 10
+    match_in_port = 109
     match_ip_eth_type = ETH_TYPE_IPv4
     match_ipv4_dst = "10.1.2.3/32"
     
     mod_nw_tos = 8
     
-    act_out_port = 15
+    act_out_port = 112
     
     print "\n"
     print ("<<< Set OpenFlow flow on the Controller")
@@ -167,7 +177,6 @@ if __name__ == "__main__":
         delete_flows(ofswitch, table_id, range(first_flow_id, flow_id+1))
         exit(0)
     
-    
     # ---------------------------------------------------
     # Second flow entry
     # ---------------------------------------------------
@@ -177,13 +186,13 @@ if __name__ == "__main__":
     priority = 600
     cookie = 1000
     
-    match_in_port = 11
+    match_in_port = 109
     match_eth_type = ETH_TYPE_IPv4
     match_ipv4_dst = "192.1.2.3/32"
     
     mod_ip_dscp = IP_DSCP_CS5
     
-    act_out_port = 15
+    act_out_port = 112
     
     print "\n"
     print ("<<< Set OpenFlow flow on the Controller")
@@ -262,4 +271,3 @@ if __name__ == "__main__":
     print (">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     print (">>> Demo End")
     print (">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    exit(0)
